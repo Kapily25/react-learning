a2f954f7ed6cf8d79c3f3bec4440ee10271d72cb
kapily25 <kapilyadav.ry25@gmail.com>
2024-11-04 13:12:30 +0530

    new

--- portfolio/src/Component/Home.jsx
+++ portfolio/src/Component/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef} from 'react'
 import pdf1 from './Data/pdf1.json'
 import mypic from './Data/mypic.json'
+import typed from 'typed.js'
 function Home() {
+  const typedRef=useRef(null)
+  useEffect(() => {
+
+    const options={
+      String:["Welcome to my ","I am Kapil","I am frontend Devloper"],
+      typespeed:50,
+      backspeed:50,
+      loop:true 
+    }
+    const typed = new typed(typedRef.current,options)
+    return () => {
+      typed.destroy()
+    };
+  }, [])
+  
   return (
     <div className="container home">
       <div className="left">
-        <h1>KAPIL HARISHCHANDRA YADAV
+        <h1 ref={typedRef}>
         </h1>
         <a href={pdf1.pdf} download="KapilResume.pdf" className="btn btn-outline-warning">Downlode Ressume</a>
       </div>
